ac38f4acfb5f12589ec7c32f9fe9858dca469b0e
sasikmrc <sasi.kmrc@gmail.com>
2021-07-26 10:26:22 +0530

    Updated in Two Branch

--- src/org/gitproject/gitProject.java
+++ src/org/gitproject/gitProject.java
@@ -8,9 +8,15 @@ public class gitProject {
 	public void empId(int id) {
 		System.out.println("Employee ID"+id);
 	}
+	
+	public void empSalary(float Salary) {
+		System.out.println("Employee Salary ="+Salary);
+
+	}
 	public static void main(String[] args) {
 		gitProject gp = new gitProject();
 		gp.emp();
 		gp.empId(100);
+		gp.empSalary(50000.2334567f);
 	}
 }
